a8a370da71ea2bd00ca28ccd517d5c05c8032200
hadidabomb2 <hadi.rizvi123@gmail.com>
2021-07-29 11:50:01 +0100

    optimising tag_manimulator

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -4,7 +4,7 @@ import re
 class TagManipulator():
     def parse_string(self, tags, regex=""):
         result = []
-        tags = tags.strip().strip(',')
+        tags = tags.strip(',')
         tempResult = re.split(regex, tags)
         tempResult = [tag.strip() for tag in tempResult]
         if(len(tempResult[0]) > 0):
